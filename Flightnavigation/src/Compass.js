4eaac2390c56e5e389b7514b2475923d52c7b0f3
Sanjana Shenoy <108812611+sanjanashenoy@users.noreply.github.com>
2024-06-01 16:09:42 +0530

    Add files via upload

---
+++ Flightnavigation/src/Compass.js
@@ -0,0 +1,10 @@
+// Compass.js
+
+import React from "react";
+import "./Compass.css"; // Import the CSS file for styling
+
+const Compass = () => {
+  return <img className="compass" src="/compass.svg"></img>;
+};
+
+export default Compass;
